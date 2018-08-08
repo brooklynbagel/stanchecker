1b71a61a020abe229fa47379e8a4898e869a8a2f
Michael Beigelmacher <brooklynbagel@gmail.com>
2018-08-07 22:25:53 -0400

    If no text is selected, grab text in current chunk

--- R/stan_checker.R
+++ R/stan_checker.R
@@ -2,6 +2,18 @@ check_stan_syntax <- function() {
   ctx <- rstudioapi::getSourceEditorContext()
   text <- rstudioapi::primary_selection(ctx)$text
 
+  if (identical(text, "")) {
+    line_num <- rstudioapi::primary_selection(ctx)$range[[1]][["row"]]
+
+    chunk_begins <- grep("^```\\{stan .*\\}$", ctx$contents)
+    chunk_begins <- max(chunk_begins[chunk_begins <= line_num])
+
+    chunk_ends <- grep("^```$", ctx$contents)
+    chunk_ends <- min(chunk_ends[chunk_ends >= line_num])
+
+    text <- ctx$contents[(chunk_begins + 1L):(chunk_ends - 1L)]
+  }
+
   fp <- tempfile()
   writeLines(text, fp)
 
